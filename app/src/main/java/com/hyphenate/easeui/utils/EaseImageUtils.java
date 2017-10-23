4d69dee58b7de629f0cd933b61e69af685c322f9
liyaqing <liyaqing@wondersgroup.com>
2017-10-23 14:18:37 +0800

    Initial commit

---
+++ app/src/main/java/com/hyphenate/easeui/utils/EaseImageUtils.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hyphenate.easeui.utils;
+
+import com.hyphenate.util.EMLog;
+import com.hyphenate.util.PathUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class EaseImageUtils extends com.hyphenate.util.ImageUtils{
+	
+	public static String getImagePath(String remoteUrl)
+	{
+		String imageName= remoteUrl.substring(remoteUrl.lastIndexOf("/") + 1, remoteUrl.length());
+		String path =PathUtil.getInstance().getImagePath()+"/"+ imageName;
+        EMLog.d("msg", "image path:" + path);
+        return path;
+		
+	}
+
+	public static String getThumbnailImagePath(String thumbRemoteUrl) {
+		String thumbImageName= thumbRemoteUrl.substring(thumbRemoteUrl.lastIndexOf("/") + 1, thumbRemoteUrl.length());
+		String path =PathUtil.getInstance().getImagePath()+"/"+ "th"+thumbImageName;
+        EMLog.d("msg", "thum image path:" + path);
+        return path;
+    }
+	/**
+	 * 根据地址获得数据的字节流
+	 * @param strUrl 网络连接地址
+	 * @return
+	 */
+	public static byte[] getImageFromNetByUrl(String strUrl){
+		try {
+			URL url = new URL(strUrl);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(5 * 1000);
+			InputStream inStream = conn.getInputStream();//通过输入流获取图片数据
+			byte[] btImg = readInputStream(inStream);//得到图片的二进制数据
+			return btImg;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * 从输入流中获取数据
+	 * @param inStream 输入流
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] readInputStream(InputStream inStream) throws Exception{
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while( (len=inStream.read(buffer)) != -1 ){
+			outStream.write(buffer, 0, len);
+		}
+		inStream.close();
+		return outStream.toByteArray();
+	}
+
+}
